af90b09cd76bfeca3814e691b846d10f448ddd39
Miklós Koren <miklos.koren@gmail.com>
2018-06-25 13:42:52 +0200

    parallel mapping of results

--- equilibrium.jl
+++ equilibrium.jl
@@ -1,3 +1,7 @@
+module ImpvolEquilibrium
+
+export period_wrapper, non_random_variable, draw_next_productivity, coerce_parameters!
+
 using Images
 using Logging
 
@@ -314,6 +318,7 @@ function outer_loop!(random_variables, parameters, t)
 end
 
 function period_wrapper(A_njs, parameters, t)
+	info("--- Period ", t, " ---")
 	random_variables = Dict{Symbol, Any}()
 	random_variables[:A_njs] = A_njs 
 	L_nj_star = outer_loop!(random_variables, parameters, t)
@@ -335,60 +340,4 @@ function check_parameters(globals)
 	@assert0 sum(globals[:alpha], 2)-1.0
 end
 
-Logging.configure(level=DEBUG)
-
-N = 10
-J = 20
-T = 1
-S = 1000
-# set random seed for reproducability
-srand(2311)
-fill_dict!(parameters, N=N, J=J, T=T, S=S)
-
-alpha = rand(J) .* ones(J,T)
-beta = 0.25 + 0.75 * rand(1, J)
-kappa_mnjt = rand(N,N,J,T)
-for j=1:J
-	for t=1:T
-		for n=1:N
-			kappa_mnjt[n,n,j,t] = 1.0
-		end
-	end
-end
-Z_njt = rand(1,N,J,T)
-
-fill_dict!(parameters, alpha=alpha ./ sum(alpha, 1), beta=beta, theta=4, kappa_mnjt=kappa_mnjt, Z_njt=Z_njt, S_nt=zeros(1,N,1,T))
-# make Gamma more diagonal
-gamma_jk = rand(J,J) + 1.0*eye(J)
-# for testing purposed, set IO links to 0
-# test for continuity with small IO links
-#parameters[:gamma_jk] = 0.75*eye(J)
-#parameters[:gamma_jk] = repmat((1-beta[:]')/J, J, 1)
-#parameters[:beta] = 0.25*ones(1,J)
-parameters[:gamma_jk] = gamma_jk ./ sum(gamma_jk, 1) .* (1-beta)
-# adaptive step size. large lambda means large steps
-parameters[:lambda] = exp(-0.05*(J-1)^0.75)
-# inverse of adjustment cost, 0 if cannot readjust
-parameters[:one_over_rho] = 0.01
-# this is log points of average input price differences
-parameters[:inner_tolerance] = 0.001
-parameters[:middle_tolerance] = 0.003
-parameters[:adjustment_tolerance] = 0.003
-parameters[:outer_tolerance] = 0.005
-parameters[:numerical_zero] = 1e-6
-
-# standard deviation for each (n,j)
-parameters[:sigma] = 0.1*ones(1,N,J,1)
-# AR coefficient for each (n,j)
-parameters[:AR_decay] = 0.9*ones(1,N,J,1)
-
-coerce_parameters!(parameters)
-
-A_njs = 1.0 .+ rand(1,N,J,S)
-
-for t = 1:T
-	info("--- Period ", t, " ---")
-	@time random_variables = period_wrapper(A_njs, parameters, t)
-	real_GDP = non_random_variable(random_variables[:real_GDP], 1)
-	A_njs = draw_next_productivity(A_njs, parameters, 1)
 end
