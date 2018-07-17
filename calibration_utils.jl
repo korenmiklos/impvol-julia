0b800322009633e17336685b2747cb380aa6c94b
balazskertesz <balazs_kertesz@yahoo.com>
2018-07-17 10:31:18 +0200

    4D array detrending proc

--- calibration_utils.jl
+++ calibration_utils.jl
@@ -1,24 +1,39 @@
-function detrend(X::Array{Float64,2}, weights::Array{Float64,1})
-	N, T = size(X)
+function detrend(X, weights::Array{Float64,1})
+	(ndims(X) == 2 || ndims(X) == 4) || error("The function 'detrend' can only be applied to 2 or 4-dimensional arrays")
+	
+	dims = ndims(X)
 
-	X_c = zeros(N,T)
-	X_t = zeros(N,T)
+	if dims == 2
+		I, S = size(X)
+		J, K = 1, 1
+	elseif dims == 4
+		I, J, K, S = size(X)
+		X = reshape(X, (I*J*K, S))
+	end
+
+	X_c = zeros(I*J*K,S)
+	X_t = zeros(I*J*K,S)
 
 	# Maximum lag
-	K = size(weights,1) - 1
+	P = size(weights,1) - 1
 	w = [flipdim(weights,1); weights[2:end]]
 
-	# Extended series with K 0's at both ends
-	X_ext = zeros(N, T + 2K)
-	X_ext[:,(K + 1):(K + T)] = X
+	# Extended series with P 0's at both ends
+	X_ext = zeros(I*J*K, S + 2P)
+	X_ext[:,(P + 1):(P + S)] = X
 
 	# Reflect head/tail
-	X_ext[:,1:K] = repeat(X[:,1], outer = [1,K]) + flipdim(repeat(X[:,1], outer = [1,K]) - X[:,2:(K + 1)],2)
-	X_ext[:,(T + K + 1):(T + 2K)] = repeat(X[:,T], outer = [1,K]) + flipdim(repeat(X[:,T],outer = [1,K]) - X[:,(T - K):(T - 1)],2)
+	X_ext[:,1:P] = repeat(X[:,1], outer = [1,P]) + flipdim(repeat(X[:,1], outer = [1,P]) - X[:,2:(P + 1)],2)
+	X_ext[:,(S + P + 1):(S + 2P)] = repeat(X[:,S], outer = [1,P]) + flipdim(repeat(X[:,S],outer = [1,P]) - X[:,(S - P):(S - 1)],2)
 
-	for t in 1:T
-		X_c[:,t] = X_ext[:,t:(t + 2K)] * w
-		X_t[:,t] = X_ext[:,t + K] - X_c[:,t]
+	for s in 1:S
+		X_c[:,s] = X_ext[:,s:(s + 2P)] * w
+		X_t[:,s] = X_ext[:,s + P] - X_c[:,s]
+	end
+
+	if dims == 4
+		X_c = reshape(X_c, (I, J, K, S))
+		X_t = reshape(X_t, (I, J, K, S))
 	end
 
 	return X_c, X_t
