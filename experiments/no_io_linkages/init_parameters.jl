99860601ba03e22ad06e6386e89ea3510cba1fc1
balazskertesz <balazs_kertesz@yahoo.com>
2018-08-03 13:45:33 +0200

    re #7, no covergence

---
+++ experiments/no_io_linkages/init_parameters.jl
@@ -0,0 +1,21 @@
+module Environment
+	export parameters
+	## these are needed for data -> parameters mapping
+	parameters = Dict{Symbol, Any}()
+
+	# CES parameters
+	parameters[:sigma] = 0.999
+	parameters[:theta] = 4.0
+	parameters[:eta] = 4.0
+
+	########## parameters common across scenarios
+	## these are function of data
+	# inverse of adjustment cost, 0 if cannot readjust
+	parameters[:one_over_rho] = 0.01
+
+	include("../config.jl")
+	# change parameters after reading data, but common across scenarios
+	## Balanced trade
+	parameters[:beta_j] = ones(size(parameters[:beta_j]))
+	parameters[:gamma_jk] = zeros(size(parameters[:gamma_jk]))
+end
