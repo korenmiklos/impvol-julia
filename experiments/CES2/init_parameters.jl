ecd1e8f61734cf54cca254db9722fdac7a5a4523
Miklós Koren <miklos.koren@gmail.com>
2018-11-01 16:36:15 +0100

    refactor: save common parameters in jld2 file

--- experiments/CES2/init_parameters.jl
+++ experiments/CES2/init_parameters.jl
@@ -6,28 +6,17 @@ using ImpvolEquilibrium, FileIO, JLD2
 parameters = Dict{Symbol, Any}()
 
 # CES parameters
-parameters[:sigma] = 0.999
+parameters[:sigma] = 2.0
 parameters[:theta] = 4.0
 parameters[:eta] = 4.0
 
 ########## parameters common across scenarios
 ## these are function of data
 # inverse of adjustment cost, 0 if cannot readjust
-parameters[:one_over_rho] = 0.0
+parameters[:one_over_rho] = 0.001
 
 include("../config.jl")
-using FileIO
-data = load("../../data/impvol_data.jld2")
 # change parameters after reading data, but common across scenarios
-## Balanced trade
-parameters[:beta_j] = ones(size(parameters[:beta_j]))
-parameters[:gamma_jk] = zeros(size(parameters[:gamma_jk]))
-parameters[:B] = CalibrateParameters.calculate_B(parameters)
-parameters[:A] = CalibrateParameters.calculate_A(parameters, data)
-CalibrateParameters.decompose_shocks!(parameters, parameters[:importance_weight])
-CalibrateParameters.draw_productivity_shocks!(parameters)
-
-parameters[:nu_njt] = CalibrateParameters.compute_alpha(parameters, data)
 # change parameters after reading data, but common across scenarios
 jldopen("common_parameters.jld2", "w") do file
 	file["parameters"] = parameters
--- experiments/baseline/init_parameters.jl
+++ experiments/baseline/init_parameters.jl
@@ -1,3 +1,7 @@
+# load ImpvolEquilibrium first so that methods are accessible
+include("../../equilibrium.jl")
+using ImpvolEquilibrium, FileIO, JLD2
+
 ## these are needed for data -> parameters mapping
 parameters = Dict{Symbol, Any}()
 
@@ -9,7 +13,10 @@ parameters[:eta] = 4.0
 ########## parameters common across scenarios
 ## these are function of data
 # inverse of adjustment cost, 0 if cannot readjust
-parameters[:one_over_rho] = 0.001
+parameters[:one_over_rho] = 0.0
 
 include("../config.jl")
 # change parameters after reading data, but common across scenarios
+jldopen("common_parameters.jld2", "w") do file
+	file["parameters"] = parameters
+end
