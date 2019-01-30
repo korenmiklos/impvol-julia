7b5f3c57cdf47d3795118bc583218471f55d07e9
Miklos Koren <miklos.koren@gmail.com>
2019-01-30 16:15:36 +0100

    Explore admissible EOS values for calibration.

--- calibrate_params.jl
+++ calibrate_params.jl
@@ -273,9 +273,15 @@ module CalibrateParameters
 		# demand shifter only varies across sectors and over time, not across countries
 		parameters[:nu_njt] = sum(country_weights .* nu, (1,2))
 
+		# enforce comformity of model with data
+		final_expenditure_shares = parameters[:nu_njt] .* (p_sectoral ./ (data["pwt"] .* P_US)) .^ (1-sigma)
+		nontradable_nu = max.(nulla, 1 .- sum(final_expenditure_shares[:,:,1:end-1,:], 3))
+		final_expenditure_shares[:,:,end:end,:] = nontradable_nu
+		final_expenditure_shares = final_expenditure_shares ./ sum(final_expenditure_shares, 3)
+
 		# step 4: calculate nontradable prices
 		# NB: DO NOT recalibrate tradable prices, expenditure_shares are very noisy for small sectors
-		if abs(sigma-1)>0.1
+		if abs(sigma-1)>0.01
 			p_sectoral[:,:,end:end,:] = data["pwt"] .* P_US .* (parameters[:nu_njt][:,:,end:end,:] ./ final_expenditure_shares[:,:,end:end,:]) .^ (1/(sigma-1))
 		else
 			p_sectoral[:,:,end:end,:] = (data["pwt"] .* P_US ./ (prod(p_sectoral[:,:,1:end-1,:] .^ parameters[:nu_njt][:,:,1:end-1,:], 3))) .^ (1 ./ parameters[:nu_njt][:,:,end:end,:])
